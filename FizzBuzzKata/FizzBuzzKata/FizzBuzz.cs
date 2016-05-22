5e26491298ff3b0b945c4de17a3c8075499a0772
Mathew James <matjamesymj@hotmail.com>
2016-05-22 18:37:08 +0100

    first commit of fizz buzz code kata

---
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzz
+    {
+        public string DoAllFizzBuzz(int howHighToGo)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < howHighToGo; i++)
+            {
+                sb.AppendFormat("{0} ", GetTextForNumber(i));
+            }
+            return sb.ToString().Trim();
+        }
+        public bool IsDivisableByThree(int input)
+        {
+            return (input % 3 == 0);
+        }
+
+        public bool IsDivisibleByFive(int input)
+        {
+            return (input % 5 == 0);
+        }
+
+        public string GetTextForNumber(int input)
+        {
+            if (IsDivisibleByFive(input) && IsDivisableByThree(input))
+            {
+                return "FizzBuzz";
+            }
+            if (IsDivisibleByFive(input))
+            {
+                return "Buzz";
+            }
+            if (IsDivisableByThree(input))
+            {
+                return "Fizz";
+            }
+            return input.ToString();
+        }
+
+
+    }
+}
---
+++ FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTests.cs
@@ -0,0 +1,69 @@
+﻿using FizzBuzzKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzKataTests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void ThreeIsDivisableByThreeTest()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            bool isDivisable = fb.IsDivisableByThree(3);
+            Assert.IsTrue(isDivisable);
+        }
+
+        [TestMethod]
+        public void ThreeIsDivisableByFiveTest()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            bool isDivisable = fb.IsDivisibleByFive(5);
+            Assert.IsTrue(isDivisable);
+        }
+
+        [TestMethod]
+        public void GetTextForNumberReturnsFizzTest()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            string returnText = fb.GetTextForNumber(3);
+            Assert.AreEqual("Fizz", returnText);
+        }
+
+        [TestMethod]
+        public void GetTextForNumberReturnsBuzzTest()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            string returnText = fb.GetTextForNumber(5);
+            Assert.AreEqual("Buzz", returnText);
+        }
+
+        [TestMethod]
+        public void GetTextForNumberReturnsFizzBuzzTest()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            string returnText = fb.GetTextForNumber(15);
+            Assert.AreEqual("FizzBuzz", returnText);
+
+        }
+
+        [TestMethod]
+        public void IsNotDivisbleByThreeOrFiveTest()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            string returnText = fb.GetTextForNumber(4);
+            Assert.AreEqual("4", returnText);
+
+        }
+
+        [TestMethod]
+        public void DoAllFizzBuzzTest()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            string returnText = fb.DoAllFizzBuzz(30);
+            Assert.AreEqual("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29", returnText);
+
+        }
+
+    }
+}
